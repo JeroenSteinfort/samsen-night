2bd7e0ac0ae27d2931b4af543534bf10b1efb1ec
JeroenSteinfort <steinfortjeroen809@gmail.com>
2016-11-28 13:09:32 +0100

    Edited menu

--- includes/menu.php
+++ includes/menu.php
@@ -1,5 +1,7 @@
 <nav class="navbar navbar-default navbar-fixed-top ">
+
     <div class="container">
+
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -13,12 +15,17 @@
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
             <ul class="nav navbar-nav">
+
                 <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
                 <li><a href="#">Partners</a></li>
                 <li><a href="#">Projecten</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
+
         </div><!-- /.navbar-collapse -->
+
     </div><!-- /.container-fluid -->
+    
 </nav>

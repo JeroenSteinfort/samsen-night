e79b3ecb4dc074ec793892f8c083d70e2cd03e45
JeroenSteinfort <steinfortjeroen809@gmail.com>
2016-11-28 12:45:35 +0100

    Edited navbar
    Deleted samsen night from navbar

--- includes/menu.php
+++ includes/menu.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Samsen Night</a>
+
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->

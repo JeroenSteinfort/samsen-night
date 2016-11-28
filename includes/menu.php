3245906dba0caa55fa8a2aa097d45e99b64b1883
JeroenSteinfort <steinfortjeroen809@gmail.com>
2016-11-28 13:52:44 +0100

    Deleted some comments

--- includes/menu.php
+++ includes/menu.php
@@ -2,7 +2,6 @@
 
     <div class="container">
 
-        <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
@@ -13,7 +12,6 @@
 
         </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
             <ul class="nav navbar-nav">
@@ -24,8 +22,8 @@
                 <li><a href="#">Contact</a></li>
             </ul>
 
-        </div><!-- /.navbar-collapse -->
+        </div>
 
-    </div><!-- /.container-fluid -->
-    
+    </div>
+
 </nav>

c3ec58e35022a7dff19e10a311ee095d7e54d977
JeroenSteinfort <steinfortjeroen809@gmail.com>
2016-12-02 11:25:55 +0100

    Error fixing

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 //Inlcudes en define base_path
 $base_path = $_SERVER['DOCUMENT_ROOT'] . "/samsen-night";
-//require_once($base_path . '/includes/password.php');
+require_once($base_path . '/includes/password.php');
 require_once($base_path . '/includes/dbh.php');
 
 session_start();

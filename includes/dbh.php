1696e16543da9419365115bd92cf5d48f7cb9444
JeroenSteinfort <steinfortjeroen809@gmail.com>
2016-12-02 11:59:20 +0100

    Fixed dbh.php

--- includes/dbh.php
+++ includes/dbh.php
@@ -4,7 +4,7 @@ $username = "u339047138_root";
 $password = "UMORFBNfQi4Yt4ikr2";
 
 try {
-    $dbh = new PDO("mysql:host=mysql.hostinger.nl;dbname=u339047138_db", $username, $password);
+    $dbh = new PDO("mysql:host=sql1.hostinger.nl;dbname=u339047138_db", $username, $password);
     // set the PDO error mode to exception
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Connected successfully";

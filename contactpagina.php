4087e12a74631531109d8f5d611e2170a751ca70
SuPaa <mathijs.breuker@hotmail.com>
2016-12-04 12:50:43 +0100

    contactpagina uitgebreid

--- contactpagina.php
+++ contactpagina.php
@@ -1,37 +1,15 @@
 <!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
     <head>
-
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link rel="stylesheet" href="css/stylesheet.css">
-        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
-
-
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
+        <meta charset="UTF-8">
+        <title></title>
     </head>
     <body>
-
-        <?php
-
-            include_once('includes/menu.php');
-
-        ?>
-
-        <div class="container container-custom">
-
-            <div class="row">
-
-                <div class="col-xs-12 content">
-                    <div class="img-wrapper">
-
-                        <img src="img/rename.png" alt="Samsen Night Logo" class="img-responsive img-logo">
-
-                    </div>
-
                     <h1>Contact pagina</h1>
                     <p>
                         mocht je een vraag hebben, of wilt gewoon een mail sturen, of je loopt gewoon te kutten ofzo boeit mij het wat stuur me gewoon een mail ik ben echt lonely help! <br> <br>
@@ -39,7 +17,7 @@
 
                     </p>
 
-                    <form method="post" action="contactpagina.php">
+                    <form method="post" action="index.php">
                         <?php if(isset($_POST["verzenden"])) {?>
                         Naam:  <input type="text" name="naam"  value= "<?php print($_POST["naam"]) ?>"> <br>
                         Email: <input type="text" name="email" value="<?php print($_POST["email"]) ?>"><br>
@@ -47,39 +25,26 @@
                     </form>
                     <?php }else{ ?>
 
-                    <form method="post" action="contactpagina.php">
+                    <form method="post" action="index.php">
+                        <table>
+                        <tr>
+                            <td>Voornaam:</td> <td><input type="text" name="voornaam"></td></tr><tr>
+                            <td>Tussenvoegsel:</td> <td><input type="text" name="tussen"></td></tr><tr>
+                            <td>Achternaam:</td> <td><input type="text" name="achternaam"></td></tr><tr>
+                            <td>bedrijfsnaam:</td><td><input type="text" name="bedrijfsnaam"></td></tr><tr>
+                            <td>Onderwerp:</td><td><input type="text" name="onderwerp"></td></tr><tr>
+                            <td>Email: </td><td><input type="text" name="email"></td></tr>
+                        
+                        
 
-                        Voornaam:  <input type="text" name="voornaam"> <br>
-                        Tussenvoegsel: <input type="text" name="tussen"> <br>
-                        Achternaam: <input type="text" name="achternaam"> <br>
-                        bedrijfsnaam: <input type="text" name="bedrijfsnaam"> <br>
-                        Onderwerp: <input type="text" name="onderwerp"> <br>
-                        Email: <input type="text" name="email"><br>
+                        
+                        </table>
+                        
                         <input type="submit" name="verzenden" value="verzenden">
                     </form>
                     <?php } ?>
+                    
 
-
-
-
-
-                </div>
-
-            </div>
-
-        </div>
-
-        <?php
-
-            include_once('includes/footer.php');
-
-        ?>
-
-        <!-- jQuery library -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-        <!-- Latest compiled JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+                    
     </body>
 </html>

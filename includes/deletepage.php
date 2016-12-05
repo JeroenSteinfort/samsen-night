df95c21e45900d9cd9afac445c8e08fc3f85846f
JeroenSteinfort <steinfortjeroen809@gmail.com>
2016-12-05 11:01:58 +0100

    Content beheren afgemaakt

---
+++ includes/deletepage.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start();
+
+$base_path = $_SERVER['DOCUMENT_ROOT'] . '\samsen-night';
+require_once($base_path . '\includes\dbh.php');
+
+if(!isset($_SESSION['logged_in'])) {
+
+    header("Location: ../index.php");
+    exit;
+
+}
+
+$error = "";
+
+if(isset($_GET['p'])){
+
+    $id = $_GET['p'];
+
+    $sql = '
+    #sql
+    DELETE FROM pagina
+    WHERE       paginaid = :id
+    ';
+    $sql = $dbh->prepare($sql);
+    $sql->bindParam(':id', $id);
+    $sql->execute();
+
+    header("Location: ../admin/pages.php");
+    exit;
+
+
+} else {
+
+    header("Location: ../admin/cpanel.php");
+    exit;
+
+}

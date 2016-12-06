1cfbb942c70ab9ceb528a33647edd857d69e0152
JeroenSteinfort <steinfortjeroen809@gmail.com>
2016-12-06 09:12:46 +0100

    Edited index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if(isset($_GET['p'])){
 
 $sql = "
 #sql
-SELECT content
+SELECT content, naam
 FROM   pagina
 WHERE  naam = :page
 LIMIT  1
@@ -81,6 +81,22 @@ if(isset($_POST['login'])) {
 
 }
 
+function check_fill($input) {
+    if (isset($_POST[$input]) && !empty($_POST[$input])) {
+        return $_POST[$input];
+    }
+    return false;
+}
+
+
+//kijken of een veld verplicht is. Zo wel, en niet ingevuld, word de naam rood.
+function required($input) {
+    if (!isset($_POST[$input]) || empty($_POST[$input])) {
+        return ' style="color:red;"';
+
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -131,6 +147,72 @@ if(isset($_POST['login'])) {
 
                         echo $contentresult[0];
 
+                        if($contentresult[1] == 'Contact'){
+
+                            if(isset($_POST["verzenden"])
+                                &&
+                                (empty($_POST['voornaam']) ||
+                                    empty($_POST['achternaam']) || empty($_POST['onderwerp']) || empty($_POST['email']) || empty($_POST['bericht']))) {?>
+                                <form method="post" action="index.php?p=Contact" id="contactpagina">
+
+                                    <table>
+                                        <tr>
+                                            <td <?php print(required("voornaam")) ?>>Voornaam:</td> <td><input required type="text" name="voornaam" value= "<?php print(check_fill("voornaam")) ?>"></td></tr><tr>
+                                            <td>Tussenvoegsel:</td> <td><input type="text" name="tussen" value= "<?php print($_POST["tussen"]) ?>"></td></tr><tr>
+                                            <td <?php print(required("achternaam")) ?>>Achternaam:</td> <td><input required type="text" name="achternaam" value= "<?php print($_POST["achternaam"]) ?>"></td></tr><tr>
+                                            <td>bedrijfsnaam:</td><td><input type="text" name="bedrijfsnaam" value= "<?php print($_POST["bedrijfsnaam"]) ?>"></td></tr><tr>
+                                            <td <?php print(required("onderwerp")) ?>>Onderwerp:</td><td><input required type="text" name="onderwerp" value= "<?php print($_POST["onderwerp"]) ?>"></td></tr><tr>
+                                            <td <?php print(required("email")) ?>>Email: </td><td><input required type="text" name="email" value= "<?php print($_POST["email"]) ?>"></td>
+                                        </tr>
+                                        <tr><td<?php print(required("bericht")) ?>>Bericht:</td></tr> <br>
+                                    </table>
+
+                                    <textarea  id="contactpagina" required form="contactpagina" name="bericht" rows="15" cols="50" wrap="soft"><?php print($_POST["bericht"]) ?></textarea><br>
+                                    <input type="submit" name="verzenden" value="verzenden">
+                                    <br>
+                                    <h3 style="color: red; text-align: center;">rode velden zijn verplicht</h3>
+                                </form>
+                            <?php }elseif(!isset($_POST['verzenden'])){ ?>
+
+                                <form method="post" action="index.php?p=Contact" id="contactpagina">
+                                    <table>
+                                        <tr>
+                                            <td>Voornaam:</td> <td><input required type="text" name="voornaam"></td></tr><tr>
+                                            <td>Tussenvoegsel:</td> <td><input  type="text" name="tussen"></td></tr><tr>
+                                            <td>Achternaam:</td> <td><input required type="text" name="achternaam"></td></tr><tr>
+                                            <td>bedrijfsnaam:</td><td><input type="text" name="bedrijfsnaam"></td></tr><tr>
+                                            <td>Onderwerp:</td><td><input required type="text" name="onderwerp"></td></tr><tr>
+                                            <td>Email: </td><td><input required type="email" name="email"></td></tr>
+                                        </tr>
+                                    </table>
+                                    Bericht: <br>
+                                    <textarea required id="contactpagina" name="bericht" rows="15" cols="50" wrap="soft"></textarea><br>
+                                    <input type="submit" name="verzenden" value="verzenden">
+
+
+                                </form>
+                            <?php }
+                            else{
+                                print("<h4>" . "Bericht is verstuurd. U ontvangt zo spoedig mogelijk een bericht." . "</h4>");
+
+                                $query = 'INSERT INTO formulier
+                                        (voornaam, tussenvoegsel,  achternaam, email, bedrijfsnaam, onderwerp, bericht)
+                                        VALUES (:voornaam, :tussenvoegsel, :achternaam, :email, :bedrijfsnaam, :onderwerp, :bericht)
+                                        ';
+
+                                $query= $dbh->prepare($query);
+                                $query->bindParam(':voornaam', $_POST['voornaam']);
+                                $query->bindParam(':tussenvoegsel', $_POST['tussen']);
+                                $query->bindParam(':achternaam', $_POST['achternaam']);
+                                $query->bindParam(':email', $_POST['email']);
+                                $query->bindParam(':bedrijfsnaam', $_POST['bedrijfsnaam']);
+                                $query->bindParam(':onderwerp', $_POST['onderwerp']);
+                                $query->bindParam(':bericht', $_POST['bericht']);
+                                $query->execute();
+                            }
+
+
+                        }
                     } else {
 
                         echo '<p>Pagina is niet gevonden</p>';

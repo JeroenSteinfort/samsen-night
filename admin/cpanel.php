b788114314b124177eff85c65e83fe7269f73996
JeroenSteinfort <steinfortjeroen809@gmail.com>
2016-12-06 10:09:45 +0100

    Fixed references

--- admin/cpanel.php
+++ admin/cpanel.php
@@ -2,8 +2,8 @@
 
 //Inlcudes en define base_path
 $base_path = $_SERVER['DOCUMENT_ROOT'] . "/samsen-night";
-require_once($base_path . '\includes\password.php');
-require_once($base_path . '\includes\dbh.php');
+require_once($base_path . '/includes/password.php');
+require_once($base_path . '/includes/dbh.php');
 session_start();
 
 if(!isset($_SESSION['logged_in'])) {

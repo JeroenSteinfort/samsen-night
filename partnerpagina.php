681ad1d0662801c9aafc942a9a7e9ce9c9e59b6b
ikbenmilanenikbenheelergblij <milann1998@gmail.com>
2016-12-07 14:21:58 +0100

    kut

--- partnerpagina.php
+++ partnerpagina.php
@@ -1,21 +1,49 @@
 <html>
 <head>
         <link rel="stylesheet" type="text/css" href="partners.css">
-     <h1>Partner pagina</h1>
 </head>
+
 <body>
-<div class="partner">
-<img src="rename.png" alt="Samsen">
-<h2>Partner 1</h2>
-<p>Beschrijving</p>
-<a href="http://www.ishetalvrijdag.nl">Link</a>
-</div>
-<br>
-<div class="partner">
-<img src="rename.png" alt="Samsen">
-<h2>Partner 2</h2>
-<p>Beschrijving</p>
-<a href="http://www.ishetalvrijdag.nl">Link</a><br>
-</div>
+
+<h1>Partner pagina</h1>
+
+<table class="partners">
+    <tr>
+        <td> </td>
+        <td>Partner 1</td>
+        <td> </td>
+    </tr>
+    <tr>
+        <td><img src="img/rename.png" alt="Samsen"></td>
+        <td>Beschrijving</td>
+        <td> </td>
+    </tr>
+    <tr>
+        <td> </td>
+        <td><a href="http://www.ishetalvrijdag.nl">Link</a></td>
+        <td> </td>
+    </tr>
+</table>
+
+<br><br>
+
+<table class="partners">
+    <tr>
+        <td> </td>
+        <td>Partner 1</td>
+        <td> </td>
+    </tr>
+    <tr>
+        <td><img src="img/rename.png" alt="Samsen"></td>
+        <td>Beschrijving</td>
+        <td> </td>
+    </tr>
+    <tr>
+        <td> </td>
+        <td><a href="http://www.ishetalvrijdag.nl">Link</a></td>
+        <td> </td>
+    </tr>
+</table>
 </body>
+
 </html>

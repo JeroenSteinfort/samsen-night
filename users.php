08c2282a600fd1b7b942f7bba3ac161433a602ba
Bobbyfanaat <woutmulder93@outlook.com>
2016-12-12 15:17:24 +0100

    Wijzigmodule

--- users.php
+++ users.php
@@ -47,35 +47,67 @@ foreach ($results as $row) {
     echo "<td class=\"cms\">" .  $row['email']  . " " . "</td>";
     echo "<td class=\"cms\">" .  $row['foto']  . " " . "</td>";
     echo "<td class=\"cms\">" .  $row['rolid']  . " " . "</td>";
-    echo "<td class=\"cms\">" . "<form action='#' method='POST'><input type='text' value='" .  $row['userid']  . "' name='userid' style='display:none;'> <input type='submit' value='Wijzig' name='wijzig' class='cmsbutton'>" .  " </input </td> </form>";
-    echo "<td class=\"cms\">" . "<form action='#' method='POST'><input type='text' value='" .  $row['userid']  . "' name='userid' style='display:none;'> <input type='submit' value='Delete' name='delete' class='cmsbutton'>" . " </input>  </td> </form> ";
+    echo "<td class=\"cms\">" . "<form action='#' method='POST'><input type='text' value='" .  $row['userid']  . "' name='userid' style='display:none;'> <input type='submit' value='Wijzig' name='wijzig' class='cmsbutton'>" . " </input> </td> </form>";
+    echo "<td class=\"cms\">" . "<form action='#' method='POST'><input type='text' value='" .  $row['userid']  . "' name='userid' style='display:none;'> <input type='submit' value='Delete' name='delete' class='cmsbutton'>" . " </input> </td> </form>";
     echo "<br> </tr>";
-    //Hier worden resultaten van de $results geshowed per regel. De delete en wijzig knop krijgen de waarde van de user id. Een input waarde word niet geshowed maar word wel gebruikt.
+    //Hier worden resultaten van de $results getoond per regel. De delete- en wijzigknop krijgen de waarde van de user-id. Een input waarde wordt niet getoont maar wordt wel gebruikt.
 }
 ?>
 </table>
+<br>
 
 <?php
 
-/*
-if (isset($_POST['wijzig'])) {
-    echo "<form method= 'POST' action= '#'>
-           <input type= 'text' name= 'vnaam' value= 'Voornaam'> " .
-         " <input type= 'text' name= 'tv' value= 'Tussenvoegsel'> " .
-         " <input type= 'text' name= 'anaam' value= 'Achternaam'> " .
-         " <input type= 'text' name= 'email' value= 'E-mail'> " .
-         " <input type= 'text' name= 'foto' value= 'Foto'> " .
-         " <input type= 'submit' value= 'Verzend' name= 'finalize' class= 'cmsbutton'> " . " </input> </form> ";
+//Bij het aanklikken van de 'wijzig' knop ontstaat de volgende vragenlijst
+if (isset($_POST['wijzig'])) { ?>
+    <form method= 'POST' action= '#'>
+          <input type= 'text' name= 'vnaam' placeholder= 'Voornaam'>
+          <input type= 'text' name= 'tv' placeholder= 'Tussenvoegsel'>
+          <input type= 'text' name= 'anaam' placeholder= 'Achternaam'>
+          <input type= 'text' name= 'email' placeholder= 'E-mail'>
+          <input type= 'text' name= 'foto' placeholder= 'Foto'>
+          <input type= 'submit' value= 'Verzend' name= 'finalize' class= 'cmsbutton'>
+          <input type= 'hidden' name= 'userid' value=" <?= $_POST['userid'] ?>">
+          </form> <?php ;
 }
 
-if(isset($_POST['finalize'])) {
-    $update = $dbh->prepare("UPDATE user SET voornaam = '$_POST['vnaam']', tussenvoegsel = '$_POST['tv']',
-                            achternaam = '$_POST['anaam']', email = '$_POST['email']', foto = '$_POST['foto']' WHERE userid = $_POST['userid']");
-    $update->execute(array($_POST['userid']));
-}*/
+if(isset($_POST['finalize']) && !empty($_POST['vnaam']) && !empty($_POST['anaam']) && !empty($_POST['email'])) {
+    echo ("Weet u zeker dat u deze wijzigingen over" . $_POST['userid'] ." wilt toepassen?");
+    ?>
+    <form method="POST" action="#">
+        <input type="submit" name="option" value="Ja" class="cmsbutton">
+        <input type="submit" name="option" value="Nee" class="cmsbutton">
+        <input type="hidden" name="userid" value="<?= $_POST['userid'] ?>">
+        <input type="hidden" name="vnaam" value="<?= $_POST['vnaam'] ?>">
+        <input type="hidden" name="tv" value="<?= $_POST['tv'] ?>">
+        <input type="hidden" name="anaam" value="<?= $_POST['anaam'] ?>">
+        <input type="hidden" name="email" value="<?= $_POST['email'] ?>">
+        <input type="hidden" name="foto" value="<?= $_POST['foto'] ?>">
+    </form>
+    <?php
+} else {
+    if (isset($_POST['finalize'])) {
+        echo ("Vul de verplichte gegevens in.");
+    }
+}
+
+if(isset($_POST['option']) && $_POST['option'] == "Ja") {
+    $update = $dbh->prepare("UPDATE user SET voornaam = :vnaam, tussenvoegsel = :tv, achternaam = :anaam, 
+                             email = :email, foto = :foto WHERE userid = :userid");
+    $update->bindParam(':userid', $_POST['userid']);
+    $update->bindValue(':vnaam', $_POST['vnaam']);
+    $update->bindValue(':tv', $_POST['tv']);
+    $update->bindValue(':anaam', $_POST['anaam']);
+    $update->bindValue(':email', $_POST['email']);
+    $update->bindValue(':foto', $_POST['foto']);
+    $update->execute();
+    header("Location: users.php");
+    exit;
+}
+
 
 if(isset($_POST['delete'])) {
-    Echo "Weet u zeker dat u de user met ID = " .  $_POST['userid']  . " wilt deleten?";
+    echo "Weet u zeker dat u de user met ID = " .  $_POST['userid']  . " wilt deleten?";
     ?>
     <form method="POST" action="#">
         <input type="submit" name="optie" value="Ja" class="cmsbutton">
@@ -89,7 +121,6 @@ if(isset($_POST['delete'])) {
 
 if (isset($_POST['optie']) && ($_POST['optie'] == "Ja")) {
     //als er op delete word geklikt wordt de onderstaande query gedraait.
-    echo 'hoi';
     $delete = $dbh->prepare("DELETE from user where userid = :userid");
     $delete->bindParam(':userid', $_POST['userid']);
     $delete->execute();

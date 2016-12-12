7b7ce35118ffb477d4197667d3d9efe758f14df5
ikbenmilanenikbenheelergblij <milann1998@gmail.com>
2016-12-12 11:58:05 +0100

    Deel SQL voor partnerpagina

--- partnerpagina.php
+++ partnerpagina.php
@@ -7,6 +7,25 @@
 
 <h1>Partner pagina</h1>
 
+<?php
+$db_servername = "localhost";
+$db_username = "root";
+$db_password = "usbw";
+$db_name = "samsen-night";
+
+// Connectie maken met database
+$conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
+// Connectie met database checken
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT partnernaam FROM partners WHERE partnerid = 1";
+$result = $conn->query($sql);
+
+?>
+
 <table class="partners">
     <tr>
         <th> </th>
@@ -15,7 +34,7 @@
     </tr>
     <tr>
         <td><img src="img/rename.png" alt="Samsen"></td>
-        <td>Dit bedrijf is gewoon echt heel erg kut want gewoon.</td>
+        <td>Dit bedrijf is gewoon echt heel erg kut want gewoon. Dit bedrijf is gewoon echt heel erg kut want gewoon. Dit bedrijf is gewoon echt heel erg kut want gewoon.</td>
         <td> </td>
     </tr>
     <tr>

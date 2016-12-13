78da9537aceb18b30d95e46ebde373b470087d18
EthanHaasjes <Ethan.Haasjes@gmail.com>
2016-12-13 09:59:16 +0100

    Super stoere footername toegevoegd.

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
 
         <div class="col-xs-12 content">
 
-            <p>&copy; Samsen Night &ndash; Powered by de placeholders
+            <p>&copy; Samsen Night &ndash; Powered by De Placeholders
                 <a href="index.php?p=algemene-voorwaarden">algemene voorwaarden</a>
             </p>
 

22887c1aacc788e5fcfdd5925eabc4b39df8617f
EthanHaasjes <Ethan.Haasjes@gmail.com>
2016-12-20 11:53:47 +0100

    footer padding

--- includes/footer.php
+++ includes/footer.php
@@ -6,19 +6,19 @@
             <div class="footer">
 
             <a href="google.nl">
-                <img src="img/stingi.png" alt="OH SHIEET">
+                <img src="img/stingi.png" class="footimg" alt="OH SHIEET">
             </a>
             <a href="google.nl">
-                <img src="img/wiggersi.png" alt="OH SHIEET">
+                <img src="img/wiggersi.png" class="footimg" alt="OH SHIEET">
             </a>
             <a href="google.nl">
-                <img src="img/rscarwashi.png" alt="OH SHIEET">
+                <img src="img/rscarwashi.png" class="footimg" alt="OH SHIEET">
             </a>
             <a href="google.nl">
-                <img src="img/knobbeni.png" alt="OH SHIEET">
+                <img src="img/knobbeni.png" class="footimg" alt="OH SHIEET">
             </a>
             <a href="google.nl">
-                <img src="img/wierdai.png" alt="OH SHIEET">
+                <img src="img/wierdai.png" class="footimg" alt="OH SHIEET">
             </a>
                 <p><br> &copy; Samsen Night &ndash; Powered by De Placeholders
                     <a href="index.php?p=algemene-voorwaarden">algemene voorwaarden</a>

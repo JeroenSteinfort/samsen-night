65c7d2c03ad1cabfaae5b3dfe891340be885f0ae
EthanHaasjes <Ethan.Haasjes@gmail.com>
2016-12-20 10:56:55 +0100

    + images en footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,11 +3,26 @@
     <div class="row">
 
         <div class="col-xs-12 content">
-
+            <div class="footer">
             <p>&copy; Samsen Night &ndash; Powered by De Placeholders
                 <a href="index.php?p=algemene-voorwaarden">algemene voorwaarden</a>
             </p>
-
+            <a href="google.nl">
+                <img src="img/stingi.png" alt="OH SHIEET">
+            </a>
+            <a href="google.nl">
+                <img src="img/wiggersi.png" alt="OH SHIEET">
+            </a>
+            <a href="google.nl">
+                <img src="img/rscarwashi.png" alt="OH SHIEET">
+            </a>
+            <a href="google.nl">
+                <img src="img/knobbeni.png" alt="OH SHIEET">
+            </a>
+            <a href="google.nl">
+                <img src="img/wierdai.png" alt="OH SHIEET">
+            </a>
+            </div>
 
         </div>
 

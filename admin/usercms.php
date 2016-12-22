b4b780f58a34b97e546460024b62da9be8c22942
SuPaa <mathijs.breuker@hotmail.com>
2016-12-22 11:28:17 +0100

    cms userbeheren actief toegevoegd

--- admin/usercms.php
+++ admin/usercms.php
@@ -107,13 +107,13 @@ include_once($base_path . '/includes/menu.php');
     <th> Tussenvoegsel </th>
     <th> Achternaam </th>
     <th> Email </th>
-    <th> Foto </th>
     <th> Rolid </th>
+            <th> actief </th>
         <th> Wijzig </th>
         <th> Delete </th> </tr>
         <?php
 
-        $userquery = $dbh->prepare("Select * from user");
+        $userquery = $dbh->prepare("Select * from user JOIN login ON user.userid = login.userid");
         $userquery->execute();
 
         $results = $userquery->fetchAll();
@@ -127,8 +127,8 @@ foreach ($results as $row) {
     echo "<td>" .  $row['tussenvoegsel']  . " " . "</td>";
     echo "<td>" .  $row['achternaam']  . " " . "</td>";
     echo "<td>" .  $row['email']  . " " . "</td>";
-    echo "<td>" .  $row['foto']  . " " . "</td>";
     echo "<td>" .  $row['rolid']  . " " . "</td>";
+    echo "<td>" .  $row['active']  . " " . "</td>";
     echo "<td>" . "<form action='admin/usercms.php' method='POST'><input type='text' value='" .  $row['userid']  . "' name='userid' style='display:none;'> <input type='submit' class=\"cms-submit\" value='Wijzig'  name='wijzig' class='cmsbutton'>" .  " </input </td> </form>";
     echo "<td>" . "<form action='admin/usercms.php' method='POST'><input type='text' value='" .  $row['userid']  . "' name='userid' style='display:none;'> <input type='submit' class=\"cms-submit\" value='Delete' name='delete' class='cmsbutton'>" . " </input>  </td> </form> ";
     echo "<br> </tr>";
@@ -145,8 +145,8 @@ if (isset($_POST['wijzig'])) { ?>
         <input type= 'text' name= 'tv' placeholder= 'Tussenvoegsel'>
         <input type= 'text' name= 'anaam' placeholder= 'Achternaam'>
         <input type= 'text' name= 'email' placeholder= 'E-mail'>
-        <input type= 'text' name= 'foto' placeholder= 'Foto'>
         <input type= 'text' name= 'rolid' placeholder= 'Rol-ID'>
+        <input type= 'text' name= 'actief' placeholder= 'actief'>
         <input type= 'submit' value= 'Verzend' name= 'finalize' class= 'cmsbutton'>
         <input type= 'hidden' name= 'userid' value=" <?= $_POST['userid'] ?>">
     </form> <?php ;
@@ -164,8 +164,9 @@ if(isset($_POST['finalize']) && !empty($_POST['vnaam']) && !empty($_POST['anaam'
         <input type="hidden" name="tv" value="<?= $_POST['tv'] ?>">
         <input type="hidden" name="anaam" value="<?= $_POST['anaam'] ?>">
         <input type="hidden" name="email" value="<?= $_POST['email'] ?>">
-        <input type="hidden" name="foto" value="<?= $_POST['foto'] ?>">
         <input type="hidden" name="rolid" value="<?= $_POST['rolid'] ?>">
+        <input type="hidden" name="actief" value="<?= $_POST['actief'] ?>">
+
     </form>
     <?php
 } else {
@@ -176,15 +177,22 @@ if(isset($_POST['finalize']) && !empty($_POST['vnaam']) && !empty($_POST['anaam'
 
 if(isset($_POST['option']) && $_POST['option'] == "Ja") {
     $update = $dbh->prepare("UPDATE user SET voornaam = :vnaam, tussenvoegsel = :tv, achternaam = :anaam, 
-                             email = :email, foto = :foto, rolid = :rolid WHERE userid = :userid");
+                             email = :email, rolid = :rolid WHERE userid = :userid");
     $update->bindParam(':userid', $_POST['userid']);
     $update->bindValue(':vnaam', $_POST['vnaam']);
     $update->bindValue(':tv', $_POST['tv']);
     $update->bindValue(':anaam', $_POST['anaam']);
     $update->bindValue(':email', $_POST['email']);
-    $update->bindValue(':foto', $_POST['foto']);
     $update->bindValue(':rolid', $_POST['rolid']);
     $update->execute();
+
+    $update2 = $dbh->prepare("UPDATE login SET active = :actief WHERE userid = :userid");
+    $update2->bindValue(':actief', $_POST['actief']);
+    $update2->bindParam("userid", $_POST['userid']);
+    $update2->execute();
+
+
+
     ?>
     <a href="http://localhost:8080/samsen-night/admin/usercms.php">Refresh de pagina</a> <?php
     exit();

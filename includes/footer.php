c7ea879d0f685b72a6c1bad07063edc8e96d05be
EthanHaasjes <Ethan.Haasjes@gmail.com>
2016-12-22 12:58:00 +0100

    MUWHAHA

--- includes/footer.php
+++ includes/footer.php
@@ -5,22 +5,22 @@
         <div class="col-xs-12 content">
             <div class="footer">
 
-            <a href="google.nl">
-                <img src="img/stingi.png" class="footimg" alt="OH SHIEET">
+            <a href="http://www.meloveradio.com">
+                <img src="img/stingi.png" class="footimg" alt="MeLoveRadio">
             </a>
-            <a href="google.nl">
-                <img src="img/wiggersi.png" class="footimg" alt="OH SHIEET">
+            <a href="http://www.wiggersmastercars.nl">
+                <img src="img/wiggersi.png" class="footimg" alt="Wiggers">
             </a>
-            <a href="google.nl">
-                <img src="img/rscarwashi.png" class="footimg" alt="OH SHIEET">
+            <a href="http://www.rscarwash.nl">
+                <img src="img/rscarwashi.png" class="footimg" alt="RSCarWash">
             </a>
-            <a href="google.nl">
-                <img src="img/knobbeni.png" class="footimg" alt="OH SHIEET">
+            <a href="http://www.knobbencaravans.nl">
+                <img src="img/knobbeni.png" class="footimg" alt="Knobben">
             </a>
-            <a href="google.nl">
-                <img src="img/wierdai.png" class="footimg" alt="OH SHIEET">
+            <a href="http://www.wierdavermogensbeheer.nl">
+                <img src="img/wierdai.png" class="footimg" alt="Wierda">
             </a>
-                <p class="footer"><br> &copy; Samsen Night &ndash; Powered by De Placeholders
+                <p class="footer"><br> &copy; Samsen Night &ndash; Powered by De Jazzhands
                     <a href="index.php?p=algemene-voorwaarden">algemene voorwaarden</a>
                 </p>
             </div>

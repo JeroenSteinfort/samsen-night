9bcc05398ce8c3e88fba3951046a8ef2916100dc
ikbenmilanenikbenheelergblij <milann1998@gmail.com>
2016-12-22 10:13:32 +0100

    Bezig met toevoeg knop

--- admin/partners.php
+++ admin/partners.php
@@ -130,6 +130,13 @@ include_once($base_path . '/includes/menu.php');
                     echo "<br> </tr>";
                     //Hier worden resultaten van de $results geshowed per regel. De delete en wijzig knop krijgen de waarde van het partner id. Een input waarde word niet geshowed maar word wel gebruikt.
                 }
+                echo \"<tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>" . "<form action='admin/partners.php' method='POST'><input type='text' value='" .  $row['partnerid']  . "' name='partnerid' style='display:none;'> <input type='submit' class=\"cms-submit\" value='Toevoegen'  name='toevoegen' class='cmsbutton'>" .  " </input> </td> </form>";
                 ?>
             <?php
 
@@ -156,6 +163,7 @@ include_once($base_path . '/includes/menu.php');
             if(isset($_POST['finalize']) && !empty($_POST['foto']) && !empty($_POST['partnernaam']) && !empty($_POST['beschrijving'])) {
             echo ("Weet u zeker dat u deze wijzigingen over ID" . $_POST['partnerid'] ." wilt toepassen?");
             ?>
+
 
             <form method="POST" action="admin/partners.php">
                 <input type="submit" name="option" value="Ja" class="cmsbutton">

7ae31a86410da3f8c3d1f565328d0332cdd53d52
SuPaa <mathijs.breuker@hotmail.com>
2016-12-22 10:20:03 +0100

    slider komt alleen op home page

--- index.php
+++ index.php
@@ -207,51 +207,12 @@ newVisitor($dbh, $page);
                     if($contentresult > 1) {
 
                         echo $contentresult[0];
-                        ?>
-                        <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                        <!-- Indicators -->
-                        <ol class="carousel-indicators">
-                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                            <li data-target="#myCarousel" data-slide-to="1"></li>
-                            <li data-target="#myCarousel" data-slide-to="2"></li>
-                            <li data-target="#myCarousel" data-slide-to="3"></li>
-                            <li data-target="#meCarousel" data-slide-to="4"></li>
-                        </ol>
 
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner" role="listbox">
-                            <div class="item active">
-                                <a href="http://www.wiggersmastercars.nl" target="_blank"><img src="img\wiggers1.png" alt="Chania">
-                            </div>
+                        if($page == "Home"){
 
-                            <div class="item">
-                                <a href="http://www.knobbencaravans.nl" target="_blank"><img src="img\knobben.png" alt="Chania">
-                            </div>
+                            require_once($base_path . '/includes/slider.php');
 
-                            <div class="item">
-                                <a href="http://www.rscarwash.nl" target="_blank"><img src="img\rscarwash.png" alt="Flower">
-                            </div>
-
-                            <div class="item">
-                                <a href="http://www.meloveradio.com" target="_blank"><img src="img\sting.jpg" alt="Chania">
-                            </div>
-
-                            <div class="item">
-                                <a href="http://www.wierdavermogensbeheer.nl" target="_blank"><img src="img\wierda.png" alt="Chania">
-                            </div>
-                        </div>
-
-                        <!-- Left and right controls -->
-                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </div>
-                    <?php
+                        }
 
                         if($contentresult['naam'] == 'Contact') {
 
